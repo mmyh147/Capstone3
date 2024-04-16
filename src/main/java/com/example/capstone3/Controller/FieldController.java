6894bde2c248aa64e193126eb1779cee0e2786f3
khaled alkuhaily <khaledalkuhaily@gmail.com>
2024-04-17 02:34:17 +0300

    added validation to field

--- src/main/java/com/example/capstone3/Controller/FieldController.java
+++ src/main/java/com/example/capstone3/Controller/FieldController.java
@@ -5,6 +5,8 @@ import com.example.capstone3.Model.Field;
 import com.example.capstone3.Service.FieldService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,23 +14,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/field")
 @RequiredArgsConstructor
 public class FieldController {
-
     private final FieldService fieldService;
+    Logger logger = LoggerFactory.getLogger(FieldController.class);
 
     @PostMapping("/add/{organizer_id}")
     public ResponseEntity addField(@PathVariable Integer organizer_id, @RequestBody @Valid Field field){
         fieldService.addField(organizer_id, field);
+        logger.info("field added");
         return ResponseEntity.ok(new ApiResponse("field added"));
     }
 
     @GetMapping("/fields")
     public ResponseEntity getAllFields(){
+        logger.info("get all fields");
         return ResponseEntity.ok(fieldService.getAllField());
     }
 
     @PutMapping("/update/{field_id}")
     public ResponseEntity updateField(@PathVariable Integer field_id, @RequestBody @Valid Field field){
         fieldService.updateField(field_id, field);
+        logger.info("field updated");
         return ResponseEntity.ok(new ApiResponse("field updated"));
     }
 
@@ -36,6 +41,7 @@ public class FieldController {
     @DeleteMapping("/delete/{field_id}")
     public ResponseEntity deleteField(@PathVariable Integer field_id){
         fieldService.deleteField(field_id);
+        logger.info("field removed");
         return ResponseEntity.ok(new ApiResponse("field deleted"));
     }
 
--- src/main/java/com/example/capstone3/Model/Field.java
+++ src/main/java/com/example/capstone3/Model/Field.java
@@ -2,13 +2,13 @@ package com.example.capstone3.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
-import java.util.Set;
-
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @Entity
 @NoArgsConstructor
@@ -16,16 +16,20 @@ public class Field {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NotEmpty(message = "name should not be empty")
+    @Size(min = 10, max = 50, message = "name should range between 10 to 50")
+    @Pattern(regexp = "^[A-Za-z]+$")
+    @Column(columnDefinition = "VARCHAR(50) NOT NULL")
     private String name;
+    @NotEmpty(message = "location should not be empty")
+    private String location;
+    @NotEmpty(message = "dimension should not be empty")
+//    @Pattern(regexp = "")
+    @Column(columnDefinition = "VARCHAR(30) not null")
     private String dimensions;
 
     @ManyToOne
     @JoinColumn(name = "organizer_id", referencedColumnName = "id")
     @JsonIgnore
     private Organizer organizer;
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "field")
-    private Set<MatchModel> matches;
-
-
 }
